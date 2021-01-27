97c8c3e8cfd31fb88a92b5f555fd095bfa6d760c
Cody Snell <codyjsnell@yahoo.com>
2021-01-27 16:47:01 -0600

    mobile styling, adds shouts component info

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,23 +3,26 @@ import './Header.css'
 const Header = ()=>{
     return(
         <div className="hero-image">
-            <img src="/images/banner.jpg" alt=""/>
-            <div className="hero-text-top">
-                <h1>Software Engineer Cody Snell</h1>
+                <img src="/images/banner.jpg" alt=""/>
+            <div className='header'>
+                <div className="hero-text-top">
+                    <h1>Cody Snell</h1>
+                    <h2>Software Engineer</h2>
+                    </div>
+                <div className="prof">
+                <img style={{width:'20%', height: '30%'}} src="/images/prof.png" alt="self"/>
                 </div>
-            <div className="prof">
-            <img src="/images/prof.jpeg" alt="self"/>
-            </div>
-            <div className="hero-text-mid">
-                <h3>Based in Victoria, Tx.</h3>
-                <h3>Available for all your website needs.</h3>
-                <h3>Email Me: <a href="mailto:codyjsnell@yahoo.com">Codyjsnell@yahoo.com</a></h3>
-                <h3>Call Me: <a href="tel:361-777-6567">361-777-6567</a></h3>
-            </div>
-            <div className="hero-text-bottom">
-                <h3>
-                    <i class="fab fa-html5"></i> HTML5 | <i class="fab fa-css3-alt"></i> CSS3 | <i class="fab fa-js-square"></i> Javascript | <i class="fab fa-react"></i> React.js | <i class="fab fa-node"></i> Node.js | <i class="fab fa-python"></i> Python | <i class="fas fa-gem"></i> Ruby on Rails 
-                </h3>
+                <div className="hero-text-mid">
+                    <h3>Based in Victoria, Tx.</h3>
+                    <h3>Available for all your website needs.</h3>
+                    <h3>Email Me: <a href="mailto:codyjsnell@yahoo.com">Codyjsnell@yahoo.com</a></h3>
+                    <h3>Call/Text Me: <a href="tel:361-777-6567">361-777-6567</a></h3>
+                </div>
+                <div className="hero-text-bottom">
+                    <h3>
+                        <i class="fab fa-html5"></i> HTML5 | <i class="fab fa-css3-alt"></i> CSS3 | <i class="fab fa-js-square"></i> Javascript | <i class="fab fa-react"></i> React.js | <i class="fab fa-node"></i> Node.js | <i class="fab fa-python"></i> Python | <i class="fas fa-gem"></i> Ruby on Rails 
+                    </h3>
+                </div>
             </div>
         </div>
     )
--- src/components/Projects/Shouts.js
+++ src/components/Projects/Shouts.js
@@ -4,13 +4,26 @@ import './Projects.css'
 function Shouts() {
     return (
         <div>
-            <h4>Shouts</h4>
+            <a href='https://socialportfolio-155d8.web.app/' target='_blank'rel="noreferrer">
+                <h1 className='siteName'>Shouts!</h1>
+            </a>
             <div className='site'>
-                <a href='https://socialportfolio-155d8.web.app/' target='_blank'>
+                <a href='https://socialportfolio-155d8.web.app/' target='_blank'rel="noreferrer">
                     <img className="siteImage" src="/images/shouts.png" alt=""/>
                 </a>
                 <div className="desc">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tenetur maxime similique rerum error provident eum assumenda ipsum vero esse, magni deserunt, animi sapiente possimus aut nulla perferendis distinctio asperiores. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, eaque debitis est eveniet quae consequatur culpa magnam neque voluptas explicabo molestias obcaecati, consectetur atque inventore molestiae? Ad ipsa accusantium est Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod nesciunt iusto error. Perspiciatis aliquid vero necessitatibus, vel, doloribus qui, voluptatem recusandae blanditiis optio neque unde odio sapiente? Praesentium, illum asperiores.</p>
+                    <p>
+                        Full stack social media app built using:
+                    </p>
+                    <ul>
+                        <li>React.js</li>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                        <li>Google Firebase</li>
+                    </ul>
+                    <p>
+                        With this website the user can signup and interact with any user on the platform.
+                    </p>
                 </div>
             </div>
         </div>
