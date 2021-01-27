02990658b0fbed3fae06da517672bf341b056696
Cody Snell <codyjsnell@yahoo.com>
2021-01-27 13:56:51 -0600

    adds proper image, sets up skeleton of display

---
+++ src/components/Projects/FindYourWiki.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Projects.css'
+
+function FindYourWiki() {
+    return (
+        <div>
+            <h4>Find Your Wiki</h4>
+            <div className='site'>
+                <a>
+                    <img className="siteImage" src="/images/find-your-wiki.png" alt=""/>
+                </a>
+                <div className="desc">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tenetur maxime similique rerum error provident eum assumenda ipsum vero esse, magni deserunt, animi sapiente possimus aut nulla perferendis distinctio asperiores. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, eaque debitis est eveniet quae consequatur culpa magnam neque voluptas explicabo molestias obcaecati, consectetur atque inventore molestiae? Ad ipsa accusantium est Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod nesciunt iusto error. Perspiciatis aliquid vero necessitatibus, vel, doloribus qui, voluptatem recusandae blanditiis optio neque unde odio sapiente? Praesentium, illum asperiores.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FindYourWiki
--- src/components/Projects/Shouts.js
+++ src/components/Projects/Shouts.js
@@ -1,1 +1,22 @@
+import React from 'react'
+import './Projects.css'
+
+function Shouts() {
+    return (
+        <div>
+            <h4>Shouts</h4>
+            <div className='site'>
+                <a href='https://socialportfolio-155d8.web.app/' target='_blank'>
+                    <img className="siteImage" src="/images/shouts.png" alt=""/>
+                </a>
+                <div className="desc">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tenetur maxime similique rerum error provident eum assumenda ipsum vero esse, magni deserunt, animi sapiente possimus aut nulla perferendis distinctio asperiores. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, eaque debitis est eveniet quae consequatur culpa magnam neque voluptas explicabo molestias obcaecati, consectetur atque inventore molestiae? Ad ipsa accusantium est Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod nesciunt iusto error. Perspiciatis aliquid vero necessitatibus, vel, doloribus qui, voluptatem recusandae blanditiis optio neque unde odio sapiente? Praesentium, illum asperiores.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Shouts
+
 
