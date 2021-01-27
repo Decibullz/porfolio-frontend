f17783940b2a36f6c0e90ff616b2b19df4f80d3e
Cody Snell <codyjsnell@yahoo.com>
2021-01-27 13:55:51 -0600

    file setup

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,24 @@
 
+import BookIt from "../Projects/BookIt"
+import CouchCritic from "../Projects/CouchCritic"
+import FindYourWiki from "../Projects/FindYourWiki"
+import Shouts from "../Projects/Shouts"
+import VeronicasLabFinder from "../Projects/VeronicasLabFinder"
 import Python from "../Python/Python"
 import "./Main.css"
 
 const Main = ()=>{
     return(
         <div className="projects">
+            <Shouts/>
             <hr/>
-            {/* <Websites/> */}
+            <CouchCritic/>
             <hr/>
+            <BookIt/>
             <hr/>
+            <VeronicasLabFinder/>
+            <hr/>
+            <FindYourWiki/>
             <hr/>
             <Python/>
             <hr/>
