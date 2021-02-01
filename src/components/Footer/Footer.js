a5fc5dd2027796ef55f90fb0f320f5ba7c9b45e2
Cody Snell <codyjsnell@yahoo.com>
2021-02-01 17:30:54 -0600

    adds resume

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,7 +14,9 @@ const Footer = ()=>{
                 <div id="socialIcons">
                     <h1 className="icons"><a href="https://www.linkedin.com/in/codysnell/" target='_blank' rel="noreferrer"><i className="fab fa-linkedin fa-lg"></i></a></h1>
                     <h1 className="icons"><a href="https://github.com/Decibullz" target='_blank' rel="noreferrer"><i className="fab fa-github-square fa-lg"></i></a></h1>
+                    <h1 className="icons"><a href="/resume.pdf" target='_blank' rel="noreferrer"><i class="far fa-file fa-lg"></i></a></h1>
                 </div>
+                
             </div>
             <footer className="Footer">Copyright&copy; Cody Snell {new Date().getFullYear()} All Rights Reserved</footer>
         </div>
