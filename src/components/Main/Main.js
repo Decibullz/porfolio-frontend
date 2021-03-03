e0499f4a9329eabbc7929e4f83547159fa4c7fcd
Cody Snell <codyjsnell@yahoo.com>
2021-03-02 18:42:45 -0600

    removed broken project

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,31 +1,25 @@
+import BookIt from '../Projects/BookIt'
+import CouchCritic from '../Projects/CouchCritic'
+import Shouts from '../Projects/Shouts'
+import VeronicasLabFinder from '../Projects/VeronicasLabFinder'
+import Python from '../Python/Python'
+import './Main.css'
 
-import BookIt from "../Projects/BookIt"
-import CouchCritic from "../Projects/CouchCritic"
-import FindYourWiki from "../Projects/FindYourWiki"
-import Shouts from "../Projects/Shouts"
-import VeronicasLabFinder from "../Projects/VeronicasLabFinder"
-import Python from "../Python/Python"
-import "./Main.css"
-
-const Main = ()=>{
-    return(
-        <div className="projects">
-            <Shouts/>
-            <hr/>
-            <CouchCritic/>
-            <hr/>
-            <BookIt/>
-            <hr/>
-            <VeronicasLabFinder/>
-            <hr/>
-            <FindYourWiki/>
-            <hr/>
-            <Python/>
-            <hr/>
-        </div>
-        
-    )}
+const Main = () => {
+  return (
+    <div className="projects">
+      <Shouts />
+      <hr />
+      <CouchCritic />
+      <hr />
+      <BookIt />
+      <hr />
+      <VeronicasLabFinder />
+      <hr />
+      <Python />
+      <hr />
+    </div>
+  )
+}
 
 export default Main
-
-
