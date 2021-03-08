ab616449a0bc2537dec77b7c10d5b18601a9d77a
Cody Snell <codyjsnell@yahoo.com>
2021-03-08 17:07:18 -0600

    imports tesla clone

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import BookIt from '../Projects/BookIt'
 import CouchCritic from '../Projects/CouchCritic'
 import Shouts from '../Projects/Shouts'
+import TeslaClone from '../Projects/TeslaClone'
 import VeronicasLabFinder from '../Projects/VeronicasLabFinder'
 import Python from '../Python/Python'
 import './Main.css'
@@ -8,6 +9,8 @@ import './Main.css'
 const Main = () => {
   return (
     <div className="projects">
+      <TeslaClone />
+      <hr />
       <Shouts />
       <hr />
       <CouchCritic />
