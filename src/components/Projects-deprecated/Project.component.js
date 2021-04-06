918472ef69bec819912f744ed7983b2357fe8e06
Cody Snell <codyjsnell@yahoo.com>
2021-04-06 18:46:37 -0500

    creates functional component that renders project based on props passed, removes need to make new component for each project

---
+++ src/components/Projects-deprecated/Project.component.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Project = ({
+  title,
+  builtWith,
+  desc,
+  desc2,
+  image,
+  inverted,
+  linkUrl,
+  gitUrl,
+  hostedBy,
+}) => {
+  return inverted ? (
+    <div>
+      <a href={linkUrl} target="_blank" rel="noreferrer">
+        <h1 className="siteName">{title}</h1>
+      </a>
+      <div className="site">
+        <div className="desc">
+          <p>
+            {desc}
+            <ul>{builtWith}</ul>
+            {desc2}
+            <p>Website is hosted with {hostedBy}</p>
+            <a target="_blank" rel="noreferrer" href={gitUrl}>
+              Github Repo
+            </a>
+          </p>
+        </div>
+        <a href={linkUrl} target="_blank" rel="noreferrer">
+          <img className="siteImage" src={image} alt="" />
+        </a>
+      </div>
+    </div>
+  ) : (
+    <div>
+      <a href={linkUrl} target="_blank" rel="noreferrer">
+        <h1 className="siteName">{title}</h1>
+      </a>
+      <div className="site">
+        <a href={linkUrl} target="_blank" rel="noreferrer">
+          <img className="siteImage" src={image} alt="" />
+        </a>
+        <div className="desc">
+          <p>
+            {desc}
+            <ul>{builtWith}</ul>
+            {desc2}
+            <p>Website is hosted with {hostedBy}</p>
+            <a target="_blank" rel="noreferrer" href={gitUrl}>
+              Github Repo
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Project
