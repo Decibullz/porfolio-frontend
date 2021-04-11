ef763e91ed12cfb006c0ca19745ddd68fb0f4aae
Cody Snell <codyjsnell@yahoo.com>
2021-04-11 00:27:11 -0500

    changes class to className

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,45 @@
-import Contact from "../Contact/Contact"
+import Contact from '../Contact/Contact'
 import './Footer.css'
 
-
-const Footer = ()=>{
-    return(
-        <div id="footerDiv">
-            <div >
-                <div id="form">
-                    <Contact/>
-                </div>
-                <div id="socialIcons">
-                    <h1 className="icons"><a href="https://www.linkedin.com/in/codysnell/" target='_blank' rel="noreferrer"><i className="fab fa-linkedin fa-lg"></i></a></h1>
-                    <h1 className="icons"><a href="https://github.com/Decibullz" target='_blank' rel="noreferrer"><i className="fab fa-github-square fa-lg"></i></a></h1>
-                    <h1 className="icons"><a href="/resume.pdf" target='_blank' rel="noreferrer"><i class="far fa-file fa-lg"></i></a></h1>
-                </div>
-                
-            </div>
-            <footer className="Footer">Copyright&copy; Cody Snell {new Date().getFullYear()} All Rights Reserved</footer>
+const Footer = () => {
+  return (
+    <div id="footerDiv">
+      <div>
+        <div id="form">
+          <Contact />
         </div>
-        
-    )}
+        <div id="socialIcons">
+          <h1 className="icons">
+            <a
+              href="https://www.linkedin.com/in/codysnell/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-linkedin fa-lg"></i>
+            </a>
+          </h1>
+          <h1 className="icons">
+            <a
+              href="https://github.com/Decibullz"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-github-square fa-lg"></i>
+            </a>
+          </h1>
+          <h1 className="icons">
+            <a href="/resume.pdf" target="_blank" rel="noreferrer">
+              <i className="far fa-file fa-lg"></i>
+            </a>
+          </h1>
+        </div>
+      </div>
+      <footer className="Footer">
+        Copyright&copy; Cody Snell {new Date().getFullYear()} All Rights
+        Reserved
+      </footer>
+    </div>
+  )
+}
 
 export default Footer
