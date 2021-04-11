0ca483c2cd8e823fa263884a1db10666a7c25420
Cody Snell <codyjsnell@yahoo.com>
2021-04-10 23:56:54 -0500

    changes to emailJS mailer from nodemailer backend

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,89 +1,101 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import './Contact.css'
+import * as emailjs from 'emailjs-com'
 
+const Contact = () => {
+  const [email, setEmail] = useState({
+    email: '',
+  })
+  const [message, setMessage] = useState({
+    message: '',
+  })
 
-const Contact = (props)=>{
+  function handleEmailChange(e) {
+    setEmail((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
 
-    const [emailState, setEmailState]= useState({
-        email: ''
-    })
-    const [message, setMessage]= useState({
-        message: ''
-    })
+  function handleMessageChange(e) {
+    setMessage((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
 
-
-    
-    function handleEmailChange(e){
-        setEmailState(prevState => ({
-            ...prevState,   
-            [e.target.name]: e.target.value
-        }))
+  const submitEmail = async (e) => {
+    e.preventDefault()
+    let templateParams = {
+      from_name: email.email,
+      to_name: process.env.REACT_APP_EMAIL,
+      message: message.message,
     }
+    emailjs
+      .send(
+        process.env.REACT_APP_SERVICE,
+        process.env.REACT_APP_TEMPLATE,
+        templateParams,
+        process.env.REACT_APP_USER
+      )
+      .then(
+        setMessage({
+          message: '',
+        })
+      )
+      .then(
+        window.confirm(
+          'Your Email has been sent!\nThank you for your interest!'
+        )
+      )
+  }
 
-    function handleMessageChange(e){
-        setMessage(prevState =>({
-            ...prevState,
-            [e.target.name]: e.target.value
-        }))
-    }
-
-    // fetch: 'http://localhost:3001/send'
-    const submitEmail = async (e)=>{
-        e.preventDefault()
-        await fetch('https://cody-snell-mailer.herokuapp.com/send', {
-            method: "POST",
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({emailState, message})
-        }).then(res => res.json()).then(setMessage({
-            message:''
-        })).then (window.confirm('Your Email has been sent!\nThank you for your interest!'))
-    }
-    
-
-    return(
-        <div className="container">
-            <div>
-                <form className="form-horizontal" onSubmit={submitEmail}>
-                    <fieldset className="fieldset">
-                    <legend>Contact Me</legend>
-                    <div className="form-group">
-                    <div id="envelope" className="col-sm-12">
-                        <h3><i className="fas fa-envelope-square fa-lg"></i></h3>
-                        <input 
-                        type='text'
-                        name='email'
-                        placeholder='Your Email Address'
-                        onChange={handleEmailChange}
-                        value={emailState.email}
-                        required
-                        />
-                    </div>
-                    </div>
-                    <div className="form-group">
-                    <div id="pencil" className="col-sm-12">
-                        <h3><i className="fas fa-pen-square fa-lg"></i></h3>
-                        <textarea
-                        name='message'
-                        type='text'
-                        placeholder="Type Your Message Here"
-                        value={message.message}
-                        onChange={handleMessageChange}
-                        required
-                        />
-                    </div>
-                    </div>
-                    <div>
-                        <button id="submit" className="btn btn-default" type='submit'>Send Message
-                        </button>
-                    </div>
-                    </fieldset>
-                </form>
+  return (
+    <div className="container">
+      <div>
+        <form className="form-horizontal" onSubmit={submitEmail}>
+          <fieldset className="fieldset">
+            <legend>Contact Me</legend>
+            <div className="form-group">
+              <div id="envelope" className="col-sm-12">
+                <h3>
+                  <i className="fas fa-envelope-square fa-lg"></i>
+                </h3>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Your Email Address"
+                  onChange={handleEmailChange}
+                  value={email.email}
+                  required
+                />
+              </div>
             </div>
-        </div>
-    )
+            <div className="form-group">
+              <div id="pencil" className="col-sm-12">
+                <h3>
+                  <i className="fas fa-pen-square fa-lg"></i>
+                </h3>
+                <textarea
+                  name="message"
+                  type="text"
+                  placeholder="Type Your Message Here"
+                  value={message.message}
+                  onChange={handleMessageChange}
+                  required
+                />
+              </div>
+            </div>
+            <div>
+              <button id="submit" className="btn btn-default" type="submit">
+                Send Message
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  )
 }
-
 
 export default Contact
