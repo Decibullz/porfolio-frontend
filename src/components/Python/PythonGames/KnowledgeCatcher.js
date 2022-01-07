9ec2382fd013ee947410cf00e8e4cda4f25bdcc1
Cody Snell <codyjsnell@yahoo.com>
2022-01-07 16:48:51 -0600

    added new resume

--- src/components/Python/PythonGames/KnowledgeCatcher.js
+++ src/components/Python/PythonGames/KnowledgeCatcher.js
@@ -1,14 +1,21 @@
-const KnowledgeCatcher = ()=>{
-    return(
+const KnowledgeCatcher = () => {
+  return (
     <div>
-        <h4>Knowledge Catcher</h4>
-        <a href="https://github.com/Decibullz/python-knowledge-catcher" target="_blank" rel="noreferrer" >
-            <img style={{width:'70%'}}src="/images/knowledge-catcher.gif" alt="KnowledgeCatcher"/>
-        </a>
-        <h5>            
-            Never ending catching game
-        </h5>
+      <h4>Knowledge Catcher</h4>
+      <a
+        href="https://github.com/Decibullz/python-knowledge-catcher"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          style={{ width: '70%' }}
+          src="/images/knowledge-catcher.gif"
+          alt="KnowledgeCatcher"
+        />
+      </a>
+      <h5>Never ending catching game</h5>
     </div>
-    )}
+  )
+}
 
 export default KnowledgeCatcher
